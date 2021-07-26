a73388b08a76299d28874f75f6b93630d7583abd
MagisterFleisher <amfleish@gmail.com>
2021-07-26 12:28:56 -0700

    created template to find max value

--- include/PrestigeSimple.hpp
+++ include/PrestigeSimple.hpp
@@ -18,41 +18,52 @@ namespace {
 using Prestige_map  =   std::unordered_map<Node, float>;
 using Alter_map     =   std::unordered_map<Node, Nodelist>;
 
+
+
+
 template<typename Add_map>
 Prestige_map PrestigeCalculate(Add_map add_map, Prestige_map prestige_map, const Alter_map& node_alters, const Nodelist& nodes)  {
     std::for_each(std::execution::par_unseq, cRANGE(nodes), [&add_map, &prestige_map, &node_alters](const auto node){
         // find the alters of the node
         const auto&                 all_alters          (node_alters.at(node));
         std::vector<Integer>        alter_degree;
+
         // find the degree of each alter
         for(auto alter : all_alters) {
             alter_degree.emplace_back(add_map.at(alter)); }
+
         prestige_map[node] = std::reduce(std::execution::par_unseq, RANGE(alter_degree)); } );
+
     return prestige_map; }
+
+
 
 
 template<typename Prestige_varied>
 Prestige_map NormalizeEigen(Prestige_varied prestige_map) {
-    std::unordered_map<Node, float> prestige_normalized;
+    std::vector<Integer>            prestige_values         {};
+    std::unordered_map<Node, float> prestige_normalized     {};
+    float                           prestige_max            (1);
+
     // Normalize the eigencentrality
     for(const auto [key, value] : prestige_map) {
         prestige_values.emplace_back(value); }
 
-    prestige_max  = prestige_values.at(std::distance(prestige_values.begin(), std::max_element(std::execution::par_unseq, RANGE(prestige_values))));
+    prestige_max  = FindMaxValue<decltype(prestige_values), float>(prestige_values);
 
     for(const auto [key, value] : prestige_map) {
         prestige_normalized[key] = value / prestige_max; }
 
-    std::cout << prestige_max << "\n";
+    std::cout << "Prestige max: " << prestige_max << "\n";
 
     return prestige_normalized; }
+
+
 
 template<typename Degree_map, typename Iter_num>
 std::unordered_map<Node, float> Prestige(const Graph& g, const Alter_map& node_alters, const Degree_map& degree_map, Iter_num iter_num) {
     // initialize a degree map with floats
     const Nodelist                  nodes               (g.nodes);
-    std::vector<Integer>            prestige_values;
-    float                           prestige_max;
     std::size_t                     iterations          (iter_num);
     Prestige_map                    degree_map_float;
     Prestige_map                    prestige_map;
@@ -69,8 +80,9 @@ std::unordered_map<Node, float> Prestige(const Graph& g, const Alter_map& node_a
             prestige_map = NormalizeEigen(PrestigeCalculate<Degree_map>(degree_map, prestige_map, node_alters, nodes));
         } else {
             // For each node, get the alters, get the prestige of the alters, add the prestige together
-            prestige_map = NormalizeEigen(PrestigeCalculate<Prestige_map>(prestige_map, prestige_map, node_alters, nodes));
-        }
-    }
-    return prestige_map; } }
+            prestige_map = NormalizeEigen(PrestigeCalculate<Prestige_map>(prestige_map, prestige_map, node_alters, nodes)); } }
+
+    return prestige_map; }
+
+}
 #endif //PANGOLIN_PRESTIGESIMPLE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
-// TODO: 
-// FIXME: 
+// TODO:
+// FIXME:
 // XXX:
-// HACK: 
+// HACK:
 // BUG:
 // CONSIDER:
 // DOING:
 // CLARIFY:
-// MARK: 
+// MARK:
 // !!!:
 // ???:
 // WARNING:
 // UNDONE:
-// 
-#pragma  GCC optimize("Ofast")
+//
+#pragma GCC optimize("Ofast")
 #include <future>
 #include <iostream>
 #include <set>
@@ -34,13 +34,15 @@
 // #include "GenNodeEdgemap.hpp"
 // #include "DisconnectedSubgraphs.hpp"
 /* I got the following timing code from someone else.  Hence the ugly used of using to simply get rid of std and whatnot */
+using std::milli;
 using std::chrono::duration;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
-using std::milli;
 
-void print_results(const char *const tag, high_resolution_clock::time_point startTime, high_resolution_clock::time_point endTime) {
-    printf("\t\t%s: Time: %fms\n\n", tag, duration_cast<duration<double, milli>>(endTime - startTime).count()); }
+void print_results(const char *const tag, high_resolution_clock::time_point startTime, high_resolution_clock::time_point endTime)
+{
+    printf("\t\t%s: Time: %fms\n\n", tag, duration_cast<duration<double, milli>>(endTime - startTime).count());
+}
 /*
 NodeAttributes GenAllNodeAttributes(const Graph& graph, const Altermap& altermap) {
     NodeAttributes      node_attributes_table   (graph.nodes.size());
@@ -69,70 +71,72 @@ NodeAttributes GenAllNodeAttributes(const Graph& graph, const Altermap& altermap
         node_attributes_table.emplace_back(         node_att); }
     return node_attributes_table; } */
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // const auto                         startTimeAll         (high_resolution_clock::now());
-    const std::string&                 file_name            (argv[1]);
-    const std::size_t&                 skip_lines           (static_cast<std::size_t>  (std::stoi(argv[2], nullptr, 10)) );  /* how many lines to skip at beginning of csv */
-    const std::uint8_t&                base_read            (static_cast<std::uint8_t> (std::stoi(argv[3], nullptr, 10)) );
+    const std::string &file_name(argv[1]);
+    const std::size_t &skip_lines(static_cast<std::size_t>(std::stoi(argv[2], nullptr, 10))); /* how many lines to skip at beginning of csv */
+    const std::uint8_t &base_read(static_cast<std::uint8_t>(std::stoi(argv[3], nullptr, 10)));
     // const std::uint8_t&                  base_write             (static_cast<std::uint8_t> (std::stoi(argv[4], nullptr, 10)) );
 
-    std::cout << "Main: File: " << file_name << "\n"; std::cout << "Main: argc: " << argc << "\n";
-    const auto                         startTimeReadCSV     (high_resolution_clock::now());
-    Graph                              raw_graph            (Graph_Read(csv, file_name, skip_lines, base_read));
-    const auto                         endTimeReadCSV       (high_resolution_clock::now());
+    std::cout << "Main: File: " << file_name << "\n";
+    std::cout << "Main: argc: " << argc << "\n";
+    const auto startTimeReadCSV(high_resolution_clock::now());
+    Graph raw_graph(Graph_Read(csv, file_name, skip_lines, base_read));
+    const auto endTimeReadCSV(high_resolution_clock::now());
 
     Summarize_Graph(raw_graph);
     print_results("ReadGraph_CSV(graph)", startTimeReadCSV, endTimeReadCSV);
 
-    const auto                         startTimeSimplify    (high_resolution_clock::now());
-    Graph                              graph                (SimplifyGraph(raw_graph));
-    const auto                         endTimeSimplify      (high_resolution_clock::now());
+    const auto startTimeSimplify(high_resolution_clock::now());
+    Graph graph(SimplifyGraph(raw_graph));
+    const auto endTimeSimplify(high_resolution_clock::now());
 
     Summarize_Graph(graph);
     print_results("SimplifyGraph(raw_graph, undirected)", startTimeSimplify, endTimeSimplify);
 
-    if(raw_graph.edges.size() == graph.edges.size()) {
+    if (raw_graph.edges.size() == graph.edges.size())
+    {
         graph.edges.clear();
         graph.edges.shrink_to_fit();
         graph.nodes.clear();
         graph.nodes.shrink_to_fit();
         // Graph& graph (raw_graph);
-        }
+    }
 
     std::cout << "\nCalling Gen_NodeAlters of graph\n";
-    const auto                              startTimeNodeAlters     (high_resolution_clock::now());
-    const auto&                             node_alters             (Gen_NodeAlters(raw_graph));
-    const auto                              endTimeNodeAlters       (high_resolution_clock::now());
+    const auto startTimeNodeAlters(high_resolution_clock::now());
+    const auto &node_alters(Gen_NodeAlters(raw_graph));
+    const auto endTimeNodeAlters(high_resolution_clock::now());
     // Summarize_NodeAlter(node_alters);
     print_results("Gen_NodeAlters(graph)", startTimeNodeAlters, endTimeNodeAlters);
 
     std::cout << "\nCalling Degree (both) of raw graph\n";
-    const auto                              startTimeDegree         (high_resolution_clock::now());
-    const std::unordered_map<Node, Integer>& node_degree            (Degree(raw_graph, both));
-    const auto                              endTimeDegree           (high_resolution_clock::now());
-//    Summarize_Nodes(node_attributes);
+    const auto startTimeDegree(high_resolution_clock::now());
+    const std::unordered_map<Node, Integer> &node_degree(Degree(raw_graph, both));
+    const auto endTimeDegree(high_resolution_clock::now());
+    //    Summarize_Nodes(node_attributes);
     print_results("Degree(raw_graph, both)", startTimeDegree, endTimeDegree);
 
     std::cout << "\nCalling in-Degree of raw graph\n";
-    const auto                              startTimeDegree_in      (high_resolution_clock::now());
-    const std::unordered_map<Node, Integer>& node_attributes_in     (Degree(raw_graph, in));
-    const auto                              endTimeDegree_in        (high_resolution_clock::now());
-//    Summarize_Nodes(node_attributes_in);
+    const auto startTimeDegree_in(high_resolution_clock::now());
+    const std::unordered_map<Node, Integer> &node_attributes_in(Degree(raw_graph, in));
+    const auto endTimeDegree_in(high_resolution_clock::now());
+    //    Summarize_Nodes(node_attributes_in);
     print_results("Degree(raw_graph, in)", startTimeDegree_in, endTimeDegree_in);
 
     std::cout << "\nCalling out-Degree of raw graph\n";
-    const auto                              startTimeDegree_out     (high_resolution_clock::now());
-    const auto                              node_attributes_out    (Degree(raw_graph, out));
-    const auto                              endTimeDegree_out       (high_resolution_clock::now());
-//    Summarize_Nodes(node_attributes_out);
+    const auto startTimeDegree_out(high_resolution_clock::now());
+    const auto node_attributes_out(Degree(raw_graph, out));
+    const auto endTimeDegree_out(high_resolution_clock::now());
+    //    Summarize_Nodes(node_attributes_out);
     print_results("Degree(raw_graph, in)", startTimeDegree_out, endTimeDegree_out);
 
-
     std::cout << "\nCalling Prestige of raw graph\n";
-    const auto node_size (raw_graph.nodes.size());
-    const auto                              startTimePrestige       (high_resolution_clock::now());
-    const auto                              node_prestige_both      (Prestige<decltype(node_degree), decltype(node_size)>(raw_graph, node_alters, node_degree, node_size));
-    const auto                              endTimePrestige         (high_resolution_clock::now());
+    const auto node_size(raw_graph.nodes.size());
+    const auto startTimePrestige(high_resolution_clock::now());
+    const auto node_prestige_both(Prestige<decltype(node_degree), decltype(node_size)>(raw_graph, node_alters, node_degree, node_size));
+    const auto endTimePrestige(high_resolution_clock::now());
     print_results("PrestigeSimple(raw_graph, in)", startTimePrestige, endTimePrestige);
     // Summarize_Prestige(node_prestige_both);
 
@@ -140,13 +144,13 @@ int main(int argc, char* argv[]) {
 
     // std::string                          write_file_name        (file_name);
     // const auto&                          write_err              (WriteGraph_CSV(graph, write_file_name, base_write));
-/*
+    /*
     std::cout << "\nCalling GenEdgemap\n";
     const auto                         startTimeEdgemap                 (high_resolution_clock::now());
     const Edgemap&                     edgemap                          (GenEdgemap(graph));
     const auto                         endTimeEdgemap                   (high_resolution_clock::now());
     print_results("GenEdgemap(graph)", startTimeEdgemap, endTimeEdgemap); */
-/*
+    /*
     std::cout << "\nCalling GenNodeEdgemap\n";
     const auto                         startTimeNodeEdgemap             (high_resolution_clock::now());
     const NodeEdgemap&                 node_edgemap                     (GenNodeEdgemap(graph));
@@ -162,4 +166,5 @@ int main(int argc, char* argv[]) {
 
     const auto                         endTimeAll              (high_resolution_clock::now());
     print_results("Total Program run: ", startTimeAll, endTimeAll); */
-    return 0; }
+    return 0;
+}
